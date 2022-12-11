15e8da76f2f229c04b9dc1eba083ba5a6294aeeb
Qube <k.twerdochlib@gmail.com>
2022-12-11 17:00:58 +0100

    up

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector("input");
+const addBtn = document.querySelector("button[data-create]");
+const removeBtn = document.querySelector("button[data-destroy]");
+const boxes = document.querySelector("#boxes");
+const removeBox = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+ input.addEventListener("input", (event) => {
+  amount = event.currentTarget.value;
+ });
+
+ addBtn.addEventListener("click", () => {
+  for (let i = 0; i < amount; i++) {
+   const newDiv = document.createElement("div");
+   boxes.append(newDiv);
+   newDiv.style.width = 30 + 10 * i + "px";
+   newDiv.style.height = 30 + 10 * i + "px";
+   newDiv.style.backgroundColor = getRandomHexColor();
+  }
+ });
+}
+
+function destroyBoxes() {
+ removeBtn.addEventListener("click", () => {
+  boxes.innerHTML = "";
+ });
+}
+
+createBoxes();
+destroyBoxes();
